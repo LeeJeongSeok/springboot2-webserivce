20157eea6c9a2f28b490ab821bf3fddaccfc491b
LeeJeongSeok <ljs0429777@gmail.com>
2020-04-15 01:35:06 +0900

    수정기능 추가

--- src/main/java/com/lee/study/service/posts/PostsService.java
+++ src/main/java/com/lee/study/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package com.lee.study.service.posts;
 
 
+import com.lee.study.domain.posts.Posts;
 import com.lee.study.domain.posts.PostsRepository;
+import com.lee.study.web.dto.PostsResponseDto;
 import com.lee.study.web.dto.PostsSaveRequestDto;
+import com.lee.study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/lee/study/web/PostApiController.java
+++ src/main/java/com/lee/study/web/PostApiController.java
@@ -1,11 +1,11 @@
 package com.lee.study.web;
 
 import com.lee.study.service.posts.PostsService;
+import com.lee.study.web.dto.PostsResponseDto;
 import com.lee.study.web.dto.PostsSaveRequestDto;
+import com.lee.study.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +16,16 @@ public class PostApiController {
     @PostMapping("/api/v1/posts")
     public long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
