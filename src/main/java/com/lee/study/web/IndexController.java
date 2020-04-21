4f7a66e2c5f0d536e368649364c6551407e8939d
LeeJeongSeok <ljs0429777@gmail.com>
2020-04-22 00:00:41 +0900

    4강 게시글 수정,삭제 기능추가

--- src/main/java/com/lee/study/web/IndexController.java
+++ src/main/java/com/lee/study/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.lee.study.web;
 
 import com.lee.study.service.posts.PostsService;
+import com.lee.study.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,5 +26,16 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        //model.addAttribute("post", dto);
+        model.addAttribute("posts", dto);
+
+        return "posts-update";
+    }
+
+
 
 }
